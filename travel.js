490eaedcfe981ab8c085eba9a81f537018f15157
Nakakandebr <bridgetnakakandea@gmail.com>
2023-05-31 08:40:26 +0300

    waterfall web

---
+++ travel.js
@@ -0,0 +1,25 @@
+
+//  const menuBtn=document.querySelector( ".menu-btn")
+//  menuBtn.addEventListener("click" ,()=>{
+    //  menuBtn.classList.toggle("active");
+    //  navigation.classList.toggle("active");
+//  });
+//  javascript for video slider navigation
+//  const btns=document.querySelectorAll(".nav-btn")
+//  const slides=document.querySelectorAll(".video-slide")
+//  var slideNav=function(manual){
+    //  btns.forEach((btn)=>{
+        //  btn.classList.remove("active")
+    //  });
+    //  slides.forEach((slide)=>{
+    //  slide.classList.remove("active")
+    //  });
+//  btns[manual].classList.add(".active");
+//  slides[manual].classList.add(".active");
+    // }
+// btns.forEach((btn, i)=>{
+    // btn.addEventListener("click", ()=>{
+        // sliderNav(i);
+    // 
+    // });
+// });
